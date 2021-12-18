ee5148148e0620aa00b9a9c9a061ebfcc8f43d2a
kleemeo <leenexer@gmail.com>
2021-12-18 15:52:30 -0500

    connect wallet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
-import * as React from "react";
+import React, { useEffect, useState } from 'react';
 // import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
 
-  const wave = () => {
+  // state to store user's public wallet.
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkWallet = async () => {
+
+    try {
+      // check metamask / ethereum object
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Do you have metamask installed?")
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      // check if authorized to access wallet
+      const accounts = await ethereum.request({ method: "eth_accounts" })
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an autheorized account: ", account)
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (err) {
+      console.err(err)
+    }
+  }
+
+  const connectWallet = async () => {
+
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0])
+    } catch (err) {
+      console.error(err)
+    }
 
   }
+
+  useEffect(() => {
+    checkWallet();
+  }, [])
 
   return (
     <div className="mainContainer">
@@ -17,12 +68,17 @@ export default function App() {
         </div>
 
         <div className="bio">
-          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am klee, this is a project tutoriallllll. YES YES YES Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {!currentAccount && (
+          <button className='waveButton' onClick={connectWallet}>Connect Wallet</button>
+        )}
+
       </div>
     </div>
   );
